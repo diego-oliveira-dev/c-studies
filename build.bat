71caeca044407dfd89b926f5e6188f6e848dee0b
Diego Oliveira <dglvr3@gmail.com>
2026-06-03 13:26:50 -0300

    chore: reorganize project structure

--- build.bat
+++ build.bat
@@ -6,9 +6,9 @@ IF NOT EXIST bin-int mkdir bin-int
 :: pushd changes the current directory to the one specified
 pushd bin-int 
 
-cl ..\random_numbers.cpp
+cl ..\current_project.cpp
 
 popd
 :: popd is the inverse of pushd: it goes back
 
-move bin-int\random_numbers.exe bin\random_numbers.exe
+move bin-int\current_project.exe bin\current_project.exe
