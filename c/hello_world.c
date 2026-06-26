4b767afc67d1f583b7f509fb886818d93b39e462
Diego <dglvr3@gmail.com>
2026-06-26 16:45:57 -0300

    chore: narrow repository scope to C studies

--- c/hello_world.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    puts("Hello, World!"); // System.out.println
-    printf("Hello, my name is %s!\n", "Diego"); // System.out.printf
-    return EXIT_SUCCESS;
-}
